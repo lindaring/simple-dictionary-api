1cd9613c9184f31138c426df02fa7c9152aaf692
Lindaring <lndsimelane@gmail.com>
2018-05-14 20:44:44 +0200

    Repo init

---
+++ src/main/java/com/lindaring/dictionary/SimpleDictionaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.lindaring.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDictionaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDictionaryApiApplication.class, args);
+	}
+
+}
