9e86ec44a7ed3b63829b6aab83e31dde9c36dbb0
Lindaring <lndsimelane@gmail.com>
2018-05-15 00:26:37 +0200

    Added log method annotation

--- src/main/java/com/lindaring/dictionary/aspect/LogAspect.java
+++ src/main/java/com/lindaring/dictionary/aspect/LogAspect.java
@@ -1,10 +1,10 @@
 package com.lindaring.dictionary.aspect;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
 import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
@@ -12,16 +12,60 @@ public class LogAspect {
 
     private static final Logger log = Logger.getLogger(LogAspect.class);
 
-    @Around("@annotation(com.lindaring.dictionary.annotation.LogExecutionTime)")
+    @Pointcut("@annotation(com.lindaring.dictionary.annotation.LogExecutionTime)")
+    public void LogExecutionTime() {
+    }
+
+    @Pointcut("@annotation(com.lindaring.dictionary.annotation.LogMethod)")
+    public void logMethod() {
+    }
+
+    @Around("LogExecutionTime()")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
+        String methodName = joinPoint.getSignature().getName();
 
         Object proceed = joinPoint.proceed();
 
         long executionTime = System.currentTimeMillis() - start;
 
-        log.info(String.format("Method '%s': completed in %s ms", joinPoint.getSignature().getName(), executionTime));
+        log.info(String.format("Method '%s': completed in %s(ms)", methodName, executionTime));
         return proceed;
+    }
+
+    @Before("logMethod()")
+    public void logMethodEntry(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+
+        String args = getArgs(joinPoint.getArgs());
+
+        log.info(String.format("Method '%s': entered with args '%s'", methodName, args));
+    }
+
+    @AfterReturning(value = "logMethod()", returning = "response")
+    public void logMethodSuccess(JoinPoint joinPoint, Object response) {
+        String methodName = joinPoint.getSignature().getName();
+
+        log.info(String.format("Method '%s': returned '%s'", methodName, response));
+    }
+
+    @AfterThrowing(value = "logMethod()", throwing = "exception")
+    public void logMethodSuccess(JoinPoint joinPoint, Throwable exception) {
+        String methodName = joinPoint.getSignature().getName();
+
+        log.error(String.format("Method '%s': throw '%s'", methodName, exception.getMessage()));
+    }
+
+    private String getArgs(Object[] args) {
+        StringBuffer argsResult = new StringBuffer();
+
+        for (int i = 0; i < args.length; i++) {
+            argsResult.append("[").append((i + 1)).append("] ").append(args[i]);
+            if ((i + 1) < args.length)
+                argsResult.append(", ");
+        }
+
+        return argsResult.toString();
     }
 
 }
--- src/main/java/com/lindaring/dictionary/controller/WordController.java
+++ src/main/java/com/lindaring/dictionary/controller/WordController.java
@@ -1,6 +1,7 @@
 package com.lindaring.dictionary.controller;
 
 import com.lindaring.dictionary.annotation.LogExecutionTime;
+import com.lindaring.dictionary.annotation.LogMethod;
 import com.lindaring.dictionary.exception.NoImplementationException;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -11,12 +12,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value="/simple-dictionary/v1/word")
 public class WordController {
 
+    @LogMethod
     @LogExecutionTime
     @RequestMapping(value="/{word}", method=RequestMethod.GET)
     @ApiOperation(notes="Get word definition", value="Get word definition")
-    public int getDefinition(@ApiParam(value="Word to search", required=true) @PathVariable String word) throws NoImplementationException, InterruptedException {
+    public int getDefinition(@ApiParam(value="Word to search", required=true) @PathVariable String word) throws InterruptedException {
         Thread.sleep(2000);
         return 100;
+    }
+
+    @LogMethod
+    @LogExecutionTime
+    @RequestMapping(value="/{word}/translate/{lang}", method=RequestMethod.GET)
+    @ApiOperation(notes="Translate the word", value="Translate the word")
+    public int getTranslation(@ApiParam(value="Word to translate", required=true) @PathVariable String word,
+                              @ApiParam(value="Language", required=true) @PathVariable String lang) throws InterruptedException {
+        Thread.sleep(1000);
+        return 100;
+    }
+
+    @LogMethod
+    @LogExecutionTime
+    @RequestMapping(value="/{word}/spelling", method=RequestMethod.GET)
+    @ApiOperation(notes="Get possible words", value="Get possible words")
+    public int getSpelling(@ApiParam(value="Word to search", required=true) @PathVariable String word) throws NoImplementationException, InterruptedException {
+        Thread.sleep(2000);
+        throw new NoImplementationException();
     }
     
 }
