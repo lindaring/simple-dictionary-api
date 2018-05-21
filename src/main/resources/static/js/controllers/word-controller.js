08fd9504a3c490e1b8bd828606a89f04978ea7a5
Lindaring <lndsimelane@gmail.com>
2018-05-22 00:04:31 +0200

    Introduced materialized css

--- src/main/resources/static/js/controllers/word-controller.js
+++ src/main/resources/static/js/controllers/word-controller.js
@@ -8,14 +8,28 @@ app.controller('wordController', function ($scope, $timeout, $route, wordService
     $scope.definition = {
         partsOfSpeech: ["asdf", "adsfasdf"]
     };
+    $scope.formEnabled = {}
+    $scope.formEnabled = {
+        definition: false,
+        preloader: false
+    };
 
     $scope.getDefinition = function () {
+        $scope.formEnabled.preloader = true;
+
         $scope.definitionPromise = wordService.getDefinition($scope.search.word);
         $scope.definitionPromise.then(function success(response) {
             $scope.definition = response.data;
+
+            $scope.formEnabled.definition = true;
+            $scope.formEnabled.preloader = false;
         }).catch(function () {
             console.log("Failed to get word definition.");
+
+            $scope.formEnabled.definition = false;
+            $scope.formEnabled.preloader = false;
         });
     };
 
+    //Todo - handle error - word not found - enable/disable form
 });
